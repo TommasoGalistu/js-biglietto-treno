d2834ab90ff5116565edb8bc53e355e512748982
TommasoGalistu <tommasogalistu92@gmail.com>
2024-06-04 14:45:44 +0200

    creazione variabile input utente

--- js/js.js
+++ js/js.js
@@ -2,9 +2,11 @@ let testoPaginaPrezzo = document.getElementById('prezzo')
 //  chiedere all’utente il numero di chilometri che vuole percorrere 
 // prompt per chiedere all'utente i chilometri
  let chilometriViaggio = parseInt(prompt('Quanti chilometri devi percorrere?'))
- 
+ console.log(typeof(chilometriViaggio), chilometriViaggio)
+
 //  chiedere l’età del passeggero.
 // prompt per chiedere l'età al passeggero
+
 
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
