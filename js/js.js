2789507c778476e043884d554d9e7aa23c30f286
TommasoGalistu <tommasogalistu92@gmail.com>
2024-06-04 14:58:22 +0200

    creazione prima condizione

--- js/js.js
+++ js/js.js
@@ -1,17 +1,27 @@
-let testoPaginaPrezzo = document.getElementById('prezzo')
+let testoPaginaPrezzo = document.getElementById('prezzo');
+let prezzoFinale;
 //  chiedere all’utente il numero di chilometri che vuole percorrere 
 // prompt per chiedere all'utente i chilometri
-let chilometriViaggio = parseInt(prompt('Quanti chilometri devi percorrere?'))
+let chilometriViaggio = parseInt(prompt('Quanti chilometri devi percorrere?'));
+
+// calcolo dei km con prezzo standard
+let costoPrezzoStandard = chilometriViaggio * 0.21;
+console.log(costoPrezzoStandard)
 
 //  chiedere l’età del passeggero.
 // prompt per chiedere l'età al passeggero
-let etaUtente = parseInt(prompt('Quanti anni hai?'))
-console.log(typeof(etaUtente), etaUtente)
+let etaUtente = parseInt(prompt('Quanti anni hai?'));
+
 
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
+if(etaUtente < 18){
+    costoPrezzoStandard -= (costoPrezzoStandard * 0.2)
+    prezzoFinale = costoPrezzoStandard.toFixed(2)
+    testoPaginaPrezzo.innerHTML = prezzoFinale
+}
 
 
 // L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
